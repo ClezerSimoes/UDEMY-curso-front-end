6d77a4b237f90f9ca9bc64df65b50e63092ac7c6
ClezerSimoes <clezer.simoes@gmail.com>
2025-07-08 11:14:54 -0300

    selecao dom

---
+++ 03_Javascript/dom/selecionando_ele_formulario/js/script.js
@@ -0,0 +1,9 @@
+function adicionarDados(){
+    const nome = document.getElementById('nome')
+    const email = document.getElementById('email')
+    const sexo = document.getElementById('sexo')
+
+    nome.value = 'Clezer'
+    email.value = 'Clezer@email.com'
+    sexo.value = 'm'
+}
--- 03_Javascript/dom/selecionando_elementos/js/script.js
+++ 03_Javascript/dom/selecionando_elementos/js/script.js
@@ -1,18 +1,40 @@
 // selecionando por ID:
 
 const obj = document.getElementById('destaque')
+obj.style.backgroundColor= "red"
 
 // Selecionando por query selector (CSS) 
 
-const objCss = document.querySelector('p.paragrafo')
-
-obj.style.backgroundColor= "red"
-
+const objCss = document.querySelector('h3.subtitulo')
 objCss.style.backgroundColor = 'blue'
 
-console.log(obj)
-console.log(objCss)
+// Selecionando por query selector All 
 
+const itens = document.querySelectorAll('li.itens')
+
+for (let item of itens){
+    item.style.backgroundColor = 'red';
+}
+
+// Selecionando por tag 
+
+const paragrafo = document.getElementsByTagName('p')
+
+paragrafo[0].style.backgroundColor = "yellow"
+paragrafo[1].style.backgroundColor = "yellow"
+
+// Selecionando por classe
+
+const laranja = document.getElementsByClassName('laranja')
+laranja[0].style.backgroundColor = 'orange'
+
+// Selecionando por classe
+
+const nome = document.getElementsByName('nome')
+nome[0].style.backgroundColor = 'green'
+
+
+// ----------------------------------------------------------------------------------------
 
 function trocarCor() {
 
